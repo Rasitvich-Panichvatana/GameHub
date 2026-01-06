667c439752bec4db8d51d0abad8822747d905d1f
Aupain <rasitvichforprogram@gmail.com>
2026-01-06 12:26:36 +0700

    Add Loading Skeletons

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
+    <Card width="300px" borderRadius={10} overflow={"hidden"}>
       <Image src={game.thumbnail} />
       <CardBody>
         <Heading fontSize="2xl">{game.title}</Heading>
